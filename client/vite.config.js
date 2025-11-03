35815527da427a2edfe5643739a97c2db52efdf1
hanskoder963 <hhm042025@gmail.com>
2025-11-03 10:40:55 +0100

    workflow and file restructure for react + vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Bytt 'Blocky-game' til nøyaktig repo-navn ditt
 export default defineConfig({
   plugins: [react()],
   base: "/Blocky-game/",
